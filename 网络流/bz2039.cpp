836a00c8f6ca32b6aa8ab248ef6f0d183c1264ac
Kewth <wthdsg@foxmail.com>
2019-01-18 17:31:34 +0800

    取消 include 模板

--- 网络流/bz2039.cpp
+++ 网络流/bz2039.cpp
@@ -1,7 +1,73 @@
-// include files {{{1
 #include <bits/stdc++.h>
-#include "template.h"
+// template {{{1
+const int maxn = 1005, maxm = 1005*1005*2;
+const int inf = 1000000000;
+int to[maxm], cap[maxm], nxt[maxm], p = 1;
+int level[maxn], head[maxn], Head[maxn];
 
+void add(int x, int y, int c) {
+	nxt[++p] = head[x];
+	to[p] = y;
+	cap[p] = c;
+	head[x] = p;
+	nxt[++p] = head[y];
+	to[p] = x;
+	cap[p] = 0;
+	head[y] = p;
+}
+
+bool bfs(int s, int t) {
+	bool res = false;
+	std::queue<int> q;
+	q.push(s);
+	memset(level, 0, sizeof(level));
+	level[s] = 1;
+	while(not q.empty()) {
+		int u = q.front();
+		q.pop();
+		if(u == t) res = true;
+		for(int i=head[u];i;i=nxt[i]) {
+			int v = to[i];
+			if(not level[v] and cap[i]) {
+				level[v] = level[u] + 1;
+				q.push(v);
+			}
+		}
+	}
+	return res;
+}
+
+int dfs(int u, int t, int c) {
+	if(u == t)
+		return c;
+	for(int &i=Head[u];i;i=nxt[i]) {
+		int v = to[i];
+		if(cap[i] and level[v] == level[u] + 1) {
+			int cc = dfs(v, t, std::min(cap[i], c));
+			if(cc) {
+				cap[i] -= cc;
+				cap[i^1] += cc;
+				return cc;
+			}
+		}
+	}
+	return 0;
+}
+
+int maxflow(int s, int t) {
+	long long res = 0;
+	while(bfs(s, t)) {
+		memcpy(Head, head, sizeof(Head));
+		int now = 0;
+		do {
+			now = dfs(s, t, inf);
+			res += now;
+		} while(now);
+	}
+	return res;
+}
+
+// Global definition {{{1
 int use[maxn], know[maxn][maxn];
 
 // Main function {{{1
