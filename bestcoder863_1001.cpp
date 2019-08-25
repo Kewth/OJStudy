4acb50dd04bdbf6a883f3fa3146bfc2d10b5fd02
Kewth <Kewth.K.D@outlook.com>
2019-08-25 16:46:35 +0800

    整理链接

--- bestcoder863_1001.cpp
+++ bestcoder863_1001.cpp
@@ -1,16 +1,1 @@
-#if 0
-2019.08.24
-#endif
-#include <cstdio>
-#define debug(...) fprintf(stderr, __VA_ARGS__)
-
-inline int input() { int x; scanf("%d", &x); return x; }
-
-int main() {
-	int T = input();
-
-	while(T --) {
-		int n = input();
-		printf("%d\n", n ^ 1);
-	}
-}
+hdu6713.cpp
